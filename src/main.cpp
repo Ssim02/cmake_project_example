e8d0fe13ed797f0dad1309f042b1d76c6a1a00e0
Ssim02 <simbojm144144144@gmail.com>
2026-06-29 23:56:38 +0900

    [2026-06-29] project setting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv) {
-    std::cout << "hello, openGL" << std::endl;
+    SPDLOG_INFO("Start program");
+
+    // glfw 라이브러리 초기화, 실패하면 에러 출력 후 종료
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description); // fmt library, 타입 지정 안해줘도 알아서 적용해줌
+        return -1;
+    }
+
+    // glfw 윈도우 생성, 실패하면 에러 출력 후 종료
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents(); // 윈도우와 관련한 여러 이벤트들을 수집하는 함수(창 크기가 늘어나던가 하는 등의 이벤트). 이거 없으면 너무 빨리 갱신되어서 멈춰있는것처럼 보일 것
+    }
+
+    glfwTerminate();
     return 0;
 }
